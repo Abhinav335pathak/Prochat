928e330eb901e1eb32853f2a6eb49f0581b2578e
Abhinav335pathak <abhinavpathak335@gmail.com>
2025-08-18 10:16:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ function MainApp() {
   const [selectedName, setSelectedName] = useState('');
 
   useEffect(() => {
-    fetch('https://prochat-e7hc.onrender.com/api/students'|| 'http://192.168.1.36:4000/', { credentials: 'include' })
+    fetch('https://prochat-e7hc.onrender.com/api/students', { credentials: 'include' })
       .then(res => res.json())
       .then(setStudents)
       .catch(console.error);
